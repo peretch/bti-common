69a167d0b4fe5eeb4bd40683e68c9b6ff48e09e4
Sebastián Pérez Etchandy <sebastian@peretch.com>
2022-07-13 11:49:54 -0300

    Updates

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,5 @@ export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
+
+// SOme comment
